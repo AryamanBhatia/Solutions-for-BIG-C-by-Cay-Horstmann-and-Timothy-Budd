c9bd3de41ed26aa45b80ff24a7840f1165c3d209
AryamanBhatia <arycodeswell@gmail.com>
2022-08-16 13:38:45 +0530

    P1.1

--- Solutions for BIG C++ by Cay Horstmann and Timothy Budd/P 1 dot 1.cpp
+++ Solutions for BIG C++ by Cay Horstmann and Timothy Budd/P 1 dot 1.cpp
@@ -13,9 +13,9 @@ using namespace std;
 int main()
 {
     cout << "Hello, my name is Hal!\n";
-    cout << "What would you like me to do?";
+    cout << "What would you like me to do?\n";
     string user_input;
     getline(cin, user_input);
-    cout << "I am sorry, I cannot do that.";
+    cout << "I am sorry, I cannot do that.\n";
     
 }
