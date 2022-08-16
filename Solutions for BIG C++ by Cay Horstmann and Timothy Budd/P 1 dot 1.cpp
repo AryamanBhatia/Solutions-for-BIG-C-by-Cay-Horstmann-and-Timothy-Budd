c3c1fde5a5dd9306b352146ed68f3f70cfb59809
AryamanBhatia <arycodeswell@gmail.com>
2022-08-16 13:34:10 +0530

    P1.1

---
+++ Solutions for BIG C++ by Cay Horstmann and Timothy Budd/P 1 dot 1.cpp
@@ -0,0 +1,21 @@
+//
+//  P 1 dot 1.cpp
+//  Solutions for BIG C++ by Cay Horstmann and Timothy Budd
+//
+//  Created by Aryaman Bhatia on 16/08/22.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, my name is Hal!\n";
+    cout << "What would you like me to do?";
+    string user_input;
+    getline(cin, user_input);
+    cout << "I am sorry, I cannot do that.";
+    
+}
---
+++ Solutions for BIG C++ by Cay Horstmann and Timothy Budd/p1.hpp
@@ -0,0 +1,13 @@
+//
+//  p1.hpp
+//  Solutions for BIG C++ by Cay Horstmann and Timothy Budd
+//
+//  Created by Aryaman Bhatia on 16/08/22.
+//
+
+#ifndef p1_hpp
+#define p1_hpp
+
+#include <stdio.h>
+
+#endif /* p1_hpp */
